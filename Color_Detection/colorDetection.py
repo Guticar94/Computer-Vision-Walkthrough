5220cf81533b62dae47a3ef79509bd7cf48ecf5f
Andres Gutierrez <guticar94@gmail.com>
2023-06-20 10:21:26 +1000

    enhancement

--- Color_Detection/colorDetection.py
+++ Color_Detection/colorDetection.py
@@ -20,20 +20,38 @@ while True:
     # Get color range limits
     lowerLimit, upperLimit = getLimits(COLOR)
     
-    # Mask pixels belonging the color we want to detect: We get all black but the color range we want to detect
+    # # Mask pixels belonging the color we want to detect: We get all black but the color range we want to detect
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
-    # Send numpy array to Pillow
-    mask_ = Image.fromarray(mask)
+# ========================== Option 1: This is preferable when there is not much noise in the image ==================================
+    # # Send numpy array to Pillow
+    # mask_ = Image.fromarray(mask)
 
-    # Get the bounding box
-    bbox = mask_.getbbox()
+    # # Get the bounding box
+    # bbox = mask_.getbbox()
 
-    # Plot the bounding box
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    # # Plot the bounding box
+    # if bbox is not None:
+    #     x1, y1, x2, y2 = bbox
+    #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+# ====================================================================================================================================
 
+# ========================== Option 2: This is larger but we can filter out small noise in the image =================================
+     # Get contours
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Print contours
+    for cnt in contours:
+        # If the contour areas are greater than 200
+        if cv2.contourArea(cnt) > 1000:
+            # Draw the contours in the original image
+            # cv2.drawContours(img, cnt, -1, (0, 0, 255), 3)
+            
+            # Get contours bounding boxes
+            x1, y1, w, h = cv2.boundingRect(cnt)
+            # Ad the bounding boxes rectangles
+            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+# ====================================================================================================================================
     # Show Image
     cv2.imshow('img', frame)
     # Close condition
